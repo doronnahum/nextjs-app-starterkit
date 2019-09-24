a4562fa3e450dd047fcc8508a519eb7d465d622d
timorss <timorss@gmail.com>
2019-09-24 14:32:26 +0300

    layout

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import Header from 'src/components/layout/Header'
+import Footer from 'src/components/layout/Footer'
+import Swiper from 'src/components/swiper/Swiper'
+import Tables from 'src/components/Tables'
+import './homeStyle.scss'
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className='home-screen'>
                 <Header />
-            </div>
+                <section className='home-screen__body'>
+                    <Swiper className='swiper-container' />
+                    <Tables className='tables'/>
+                    <div style={{
+                        height: 100,
+                        backgroundColor: 'lightblue',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        display: 'flex'
+                    }}>
+                        start save today
+                    </div>
+                </section>
+                <Footer />
+            </div >
         )
     }
 }
