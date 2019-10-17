0dc22a4babb7b5fc43ec96e895a89cc50a1e0425
doronnahum <doron.nahum@gmail.com>
2019-10-17 22:37:49 +0300

    add utils

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import result from 'lodash/result';
+
+export function isStringEmpty(text) {
+  return !text || text === '' || text.trim() === '';
+}
+
+export function isEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+export const getDeep = result;
