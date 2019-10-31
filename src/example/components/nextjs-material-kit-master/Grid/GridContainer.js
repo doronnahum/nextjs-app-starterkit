8333bf7169dc5282aaf0abc08c3d6503ceab10d6
doron nahum <doron.nahum@gmail.com>
2019-10-31 11:05:27 +0200

    add pre-commit": "lint-staged fix localButtonIssue

--- src/example/components/nextjs-material-kit-master/Grid/GridContainer.js
+++ src/example/components/nextjs-material-kit-master/Grid/GridContainer.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/require-default-props */
+/* eslint-disable */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 // nodejs library to set properties for components
